21928a52e938bd8700502ec5548a9b9ff5dadc99
webistomin <webistomin@gmail.com>
2018-08-18 10:52:16 +0300

    Попробовал event delegation

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,19 +1,30 @@
 var li = document.querySelectorAll('.movies__item');
+var list = document.querySelectorAll('.movies__list');
 var popup = document.querySelector('.popup');
 var btn = document.querySelector('.popup__button');
 
-li.forEach(function (item) {
-  item.addEventListener('click', function() {
-    if (popup.classList.contains('popup--closed')) {
-      popup.classList.remove('popup--closed');
-      popup.classList.add('popup--opened');
-      document.body.style.overflow = 'hidden';
-    } else {
-      popup.classList.add('popup--closed');
-      popup.classList.remove('popup--opened');
-    }
+list.forEach(function (item) {
+  item.addEventListener('click', function (e) {
+    var a = event.target.closest('a');
+
+    if (!a) return;
+
+    if (!item.contains(a)) return;
+
+    openPopup(a); // highlight it
   });
 });
+
+function openPopup(a) {
+  if (popup.classList.contains('popup--closed')) {
+    popup.classList.remove('popup--closed');
+    popup.classList.add('popup--opened');
+    document.body.style.overflow = 'hidden';
+  } else {
+    popup.classList.add('popup--closed');
+    popup.classList.remove('popup--opened');
+  }
+}
 
 btn.addEventListener('click', function() {
   if (popup.classList.contains('popup--opened')) {
